2c2046f1c35354b07abe21c34de291db40364450
Joi Macedo <joi.macedo@hotmail.com>
2021-11-10 07:36:55 -0300

    primeiro endpoint

--- src/main/java/br/com/joi/ecommerce/resources/CategoriaResource.java
+++ src/main/java/br/com/joi/ecommerce/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.joi.ecommerce.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joi.ecommerce.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informatica");
+		Categoria categoria2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 	
 
