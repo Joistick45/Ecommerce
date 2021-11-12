f7b5e94b4e08f0663afad7801d9f697c0b54d13a
Joi Macedo <joi.macedo@hotmail.com>
2021-11-12 07:04:33 -0300

    primeiro relacionamento das tabelas

--- src/main/java/br/com/joi/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/joi/ecommerce/EcommerceApplication.java
@@ -1,13 +1,50 @@
 package br.com.joi.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceApplication {
+import br.com.joi.ecommerce.domain.Categoria;
+import br.com.joi.ecommerce.domain.Produto;
+import br.com.joi.ecommerce.repositories.CategoriaRepository;
+import br.com.joi.ecommerce.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class EcommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+			
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
 	}
 
 }
